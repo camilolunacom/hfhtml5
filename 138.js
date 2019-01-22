2bd5ce7dcf514d7ba2f223910272edd8aa9d329f
Camilo Luna <camilo@camiloluna.com>
2019-01-21 21:09:59 -0500

    Add exercises

---
+++ 138.js
@@ -0,0 +1,13 @@
+var movie1 = {
+    name: "Plan 9 from Outer Space",
+    shows: ["3:00pm, 7:00pm, 11:00pm"],
+    genre: "cult classic",
+    rating: 2
+};
+
+var movie2 = {
+    name: "Forbidden Planet",
+    shows: ["5:00pm, 9:00pm"],
+    genre: "classic sci-fi",
+    rating: 5
+};
